207282d0469df1d14493ce876d42c6b2b81c26d6
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-03-22 17:59:39 -0400

    update active navigation bar

--- public/application.js
+++ public/application.js
@@ -1,12 +1,15 @@
 var myApp = angular.module('cuongApp', ['ngRoute']);
 
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-            $routeProvider
+    $locationProvider.hashPrefix('!');
+    $routeProvider
                 .when('/', {templateUrl: 'public/navbar/views/home.html'})
-            .when('/profile', {templateUrl: 'public\\navbar\\views\\profile.html'})
+            .when('/profile', {templateUrl: 'public/navbar/views/profile.html'})
             .when('/projects', {templateUrl: 'public/navbar/views/projects.html'})
             .otherwise({redirectTo: '/'});
 
             // $locationProvider.html5Mode({enabled: true, requireBase: false});
+
+
 }]);
 
