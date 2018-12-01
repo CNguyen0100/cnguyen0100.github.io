ce3e2e237739e49ee8fe1e9bcb115f2e6986c866
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-12-01 15:24:41 -0500

    update new website

--- public/navbar/controllers/navBarController.js
+++
@@ -1,5 +0,0 @@
-angular.module('cuongApp').controller('navController', ['$scope', '$location', function($scope, $location){
-    $scope.isActive = function(destination){
-        return destination === $location.path();
-    }
-}]);
